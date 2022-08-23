609cbe341611455fffd394619069c124534d0f8d
ajkn1904 <ajknishat@gmail.com>
2022-08-23 20:38:57 +0600

    update

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,15 @@
-const selectVHeaderNumber = document.getElementById('selected-v-header');
-const playerExpenseText = document.getElementById('player-expense');
-
 function afterAddToList(element){
     element.disabled = true;
   }
 
-// -----------------creating li and inserting them to select-v div------------------
+
+
+const selectVHeaderNumber = document.getElementById('selected-v-header');
+const playerExpenseText = document.getElementById('player-expense');
+
+
+
+// ---------creating li and inserting them to select-v div--------
 const btns = document.querySelectorAll('.contentsDivBtn');
 
 btns.forEach(contentsDivBtn => {
@@ -27,36 +31,57 @@ contentsDivBtn.addEventListener('click', function(event){
    });
 });
 
+
+
 document.getElementById('calculate-btn').addEventListener('click', function(){
     const selectedNumber = parseInt(selectVHeaderNumber.innerText);
 
     const perPlayer = document.getElementById('per-player');
-    const perPlayerCost = parseFloat(perPlayer.value);
+    const perPlayerCostString = perPlayer.value;
+    
 
-    const playerExpense = playerExpenseText.innerText;
+    if(isNaN(perPlayerCostString) || (perPlayerCostString == '')){
+        alert("Please enter a number.");
+    }else{
+        const perPlayerCost = parseFloat(perPlayerCostString)
+    
+        const playerExpense = playerExpenseText.innerText;
+    
+        const playerExpenses  = perPlayerCost * selectedNumber;
+    
+        playerExpenseText.innerText = playerExpenses;
+    }
 
-    const playerExpenses  = perPlayerCost * selectedNumber;
-
-    playerExpenseText.innerText = playerExpenses;
 
     perPlayer.value ='';
 
 });
 
+
+
 document.getElementById('calculate-total-btn').addEventListener('click', function(){
     const playerExpense = parseFloat(playerExpenseText.innerText);
 
     const manegerCost = document.getElementById('maneger');
-    const manegerCostValue = parseFloat(manegerCost.value);
+    const manegerCostValueString = manegerCost.value;
 
     const coachCost = document.getElementById('coach');
-    const coachCostValue = parseFloat(coachCost.value);
+    const coachCostValueString = coachCost.value;
 
-    const getTotalText =  document.getElementById('total');
+
+    if(isNaN(manegerCostValueString) || isNaN(coachCostValueString) || (manegerCostValueString=='') || (coachCostValueString=='')){
+        alert("Please enter a number.");  
+    }
+    else{
+        const manegerCostValue = parseFloat(manegerCostValueString);
+        const coachCostValue = parseFloat(coachCostValueString);
+        const getTotalText =  document.getElementById('total');
+        
+        const totalExpenses = playerExpense + manegerCostValue + coachCostValue;
     
-    const totalExpenses = playerExpense + manegerCostValue + coachCostValue;
+        getTotalText.innerText = totalExpenses;
+    }
 
-    getTotalText.innerText = totalExpenses;
 
     manegerCost.value ='';
     coachCost.value ='';
